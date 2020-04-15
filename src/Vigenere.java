8697af546a05a666866e556a180ac7df4f90e33f
Jiaxuan Wang <jiaxuanw1@gmail.com>
2020-04-15 03:37:19 -0400

    Fix misleading variable names
    Switched StringBuilder names "ciphertext" and "plaintext" in encrypt/decrypt methods

--- src/Vigenere.java
+++ src/Vigenere.java
@@ -12,7 +12,7 @@ public class Vigenere {
 	 * 
 	 * @param key    the key word/phrase
 	 * @param length the desired length for the key
-	 * @return
+	 * @return the generated key
 	 */
 	private static String generateKey(String key, int length) {
 		StringBuilder keyString = new StringBuilder(key);
@@ -38,14 +38,14 @@ public class Vigenere {
 		key = key.toUpperCase().replace(" ", "");
 		String keystream = generateKey(key, text.length());
 
-		StringBuilder plaintext = new StringBuilder();
+		StringBuilder ciphertext = new StringBuilder();
 		for (int i = 0; i < text.length(); i++) {
 			int letterVal = (int) text.charAt(i) - 65;
 			int keyShift = (int) keystream.charAt(i) - 65;
-			plaintext.append((char) ((letterVal + keyShift) % 26 + 65));
+			ciphertext.append((char) ((letterVal + keyShift) % 26 + 65));
 		}
 
-		return plaintext.toString();
+		return ciphertext.toString();
 	}
 
 	/**
@@ -60,14 +60,14 @@ public class Vigenere {
 		key = key.toUpperCase().replace(" ", "");
 		String keystream = generateKey(key, text.length());
 
-		StringBuilder ciphertext = new StringBuilder();
+		StringBuilder plaintext = new StringBuilder();
 		for (int i = 0; i < text.length(); i++) {
 			int letterVal = (int) text.charAt(i) - 65;
 			int keyShift = (int) keystream.charAt(i) - 65;
-			ciphertext.append((char) ((letterVal - keyShift + 26) % 26 + 65));
+			plaintext.append((char) ((letterVal - keyShift + 26) % 26 + 65));
 		}
 
-		return ciphertext.toString();
+		return plaintext.toString();
 	}
 
 }
